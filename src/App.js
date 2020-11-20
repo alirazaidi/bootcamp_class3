5bf1cf69c5471000c1d8a6fc382d2e0d43d7e3fe
alirazaidi <alirazaidi@hotmail.com>
2020-11-20 12:48:19 +0500

    added new method
    added new method Students

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+
 import './App.css';
-import Students from './student.js';
+import Students from './Students.js';
 
 function App() {
   return (
-    <div className='App'>
-    <Students StudentRollno='10'/>
-    <Students StudentName='Syed ali Raza Zaidi'/>
-    <Students StudentAge='40'/>
+    <div className="App">
+    <Students StudentRollno="10"/>
+    <Students StudentName="Syed ali Raza Zaidi"/>
+    <Students StudentAge="40"/>
     </div>
+    
   );
 }
 
---
+++ src/Students.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function Students(props)
+{
+return 
+    (
+    /*<div>
+        <h1>Student Name {props.StudentRollno}</h1>
+        /*<h1>Student Name {props.StudentAge}</h1>
+        <h1>Student Name {props.StudentName}</h1>
+    </div>*/
+    <h1>Student Name {props.StudentName}</h1>
+    );
+}
+export default Students;
